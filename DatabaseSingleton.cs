030626cf638b2c6a577e293e7410f08a3d816428
naim90 <naim.elmoudni@gmail.com>
2021-11-22 10:54:00 +0100

    quest Singleton

---
+++ DatabaseSingleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class DatabaseSingleton
+    { 
+        private DatabaseSingleton() { }
+
+        private static DatabaseSingleton _instance;
+
+        public static DatabaseSingleton GetInstance()
+        {
+            if(_instance==null)
+            {
+                _instance = new DatabaseSingleton();
+            }
+            return _instance;
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Titi a = new Titi();
+            Titi b = new Titi();
+
+        }
+    }
+}
